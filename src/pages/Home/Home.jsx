b76e32933fec4f2a0c91c441bf93af89d507abb2
Charanraju18 <charanraju925@gmail.com>
2024-08-25 23:42:24 +0530

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Content from '../../components/Content/Content'
+import Footer from '../../components/Footer/Footer'
 function Home() {
   return (
 	<div>
 		<Navbar/>
 		<Content/>
+		<Footer/>
 	</div>
   )
 }
