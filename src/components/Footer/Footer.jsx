9ed907b9a1574c6e3f6e188e4cb9da3599b1c826
Charanraju18 <charanraju925@gmail.com>
2024-08-26 00:03:45 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF,faInstagram,faYoutube,faXTwitter,faLinkedin } from '@fortawesome/free-brands-svg-icons'
 function Footer() {
   return (
 	<div className='footer'>
-    
+    <FontAwesomeIcon icon={faFacebookF} style={{color: "#ffffff",}} />
+    <FontAwesomeIcon icon={faInstagram} style={{color: "#ffffff",}} />
+    <FontAwesomeIcon icon={faYoutube} style={{color: "#ffffff",}} />
+    <FontAwesomeIcon icon={faXTwitter} style={{color: "#ffffff",}} />
+    <FontAwesomeIcon icon={faLinkedin} style={{color: "#ffffff",}} />
   </div>
   )
 }
