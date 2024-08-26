70ce81d5eb6a491a8ea49e8ca40b7e5e8e5fd5ff
Charanraju18 <charanraju925@gmail.com>
2024-08-26 09:45:44 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF,faInstagram,faYoutube,faXTwitter,faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import logo from '../../../public/logo.png'
 function Footer() {
   return (
 	<div className='footer'>
-    <FontAwesomeIcon icon={faFacebookF} style={{color: "#ffffff",}} />
-    <FontAwesomeIcon icon={faInstagram} style={{color: "#ffffff",}} />
-    <FontAwesomeIcon icon={faYoutube} style={{color: "#ffffff",}} />
-    <FontAwesomeIcon icon={faXTwitter} style={{color: "#ffffff",}} />
-    <FontAwesomeIcon icon={faLinkedin} style={{color: "#ffffff",}} />
+    <div className='icons'>
+    <FontAwesomeIcon icon={faFacebookF} style={{color: "#ffffff",}} className='social_media_icons'/>
+    <FontAwesomeIcon icon={faInstagram} style={{color: "#ffffff",}} className='social_media_icons' />
+    <FontAwesomeIcon icon={faYoutube} style={{color: "#ffffff",}} className='social_media_icons' />
+    <FontAwesomeIcon icon={faXTwitter} style={{color: "#ffffff",}} className='social_media_icons' />
+    <FontAwesomeIcon icon={faLinkedin} style={{color: "#ffffff",}} className='social_media_icons' />
+    </div>
+    <hr />
+    <div className='footer_logo'>
+      <hr className='logo_hr hr1'/>
+    <a  href="#"><img src={logo} width={100} height={90} style={{margin: "auto"}}/></a>
+      <hr className='logo_hr hr2'/>
+    </div>
   </div>
   )
 }
